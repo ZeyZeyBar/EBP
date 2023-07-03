aa0cd6a5a99785113531d3fc2cee78562cfd58ac
Zeynep BARAN <zbaran@outlook.com.tr>
2023-07-03 20:31:41 +0300

    admin giriş ve yönlendirme yapıldı.

---
+++ EBP.WebUI/Areas/User/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using EBP.Core.Service;
+using EBP.Model.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EBP.WebUI.Areas.User.Controllers
+{
+    [Authorize]
+    [Area("User")]
+    public class AdminController : Controller
+    {
+        private readonly ICoreService<PersonelUser> _personelUser;
+        private readonly ICoreService<Personel> _personel;
+        private readonly ICoreService<Department> _department;
+        private readonly ICoreService<Brand> _brand;
+
+        public AdminController(ICoreService<PersonelUser> personelUser, ICoreService<Personel> personel,ICoreService<Department> department,ICoreService<Brand> brand)
+        {
+            _brand = brand;
+            _personelUser = personelUser;
+            _personel = personel;
+            _department = department;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
