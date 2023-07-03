85a5bf936448488691fd1a516febab9ddcae6ba1
Zeynep BARAN <zbaran@outlook.com.tr>
2023-07-03 21:32:22 +0300

    Departman ekle, sil, güncelleme ve listeleme yapıldı.

---
+++ EBP.WebUI/Areas/User/Models/AdminDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EBP.WebUI.Areas.User.Models
+{
+    public class AdminDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
